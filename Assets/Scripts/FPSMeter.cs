10156fedc8136996f4b4ee3dc681b6015a42436e
nazarov.andrey <nazarov.andrey@me.com>
2017-05-30 10:44:17 +0300

    input handling optimized for mobile; tested on devices

---
+++ Assets/Scripts/FPSMeter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSMeter : MonoBehaviour
+{
+	[SerializeField]
+	private Text text;
+	private float time = 0f;
+	private int frames = 0;
+
+	private void Awake ()
+	{
+		Application.targetFrameRate = 30;
+	}
+
+	private void Update ()
+	{
+		time += Time.deltaTime;
+		frames++;
+
+		if (time >= 1f) {			
+			text.text = Mathf.RoundToInt ((float)frames / time).ToString ();
+			time = 0f;
+			frames = 0;
+		}
+	}
+}
--- Assets/Scripts/TextureEraser.cs
+++ Assets/Scripts/TextureEraser.cs
@@ -35,6 +35,17 @@ public class TextureEraser : MonoBehaviour
 
 	private void Update ()
 	{
+		bool pressed = Input.GetMouseButton (0);
+		bool released = Input.GetMouseButtonUp (0);
+
+		if (released) {
+			brushImage.gameObject.SetActive (false);
+			return;
+		}
+
+		if (!pressed)
+			return;
+
 		brushImage.rectTransform.position = Input.mousePosition;
 
 		Vector3[] brushWorldCorners = new Vector3[4];
@@ -44,28 +55,26 @@ public class TextureEraser : MonoBehaviour
 			brushWorldCorners [2].x, brushWorldCorners [2].y);
 
 		bool brushAboveTexture = textureImageRect.Overlaps (brushWorldRect);
-		brushImage.gameObject.SetActive (brushAboveTexture);
 		if (!brushAboveTexture)
 			return;
 
-		if (Input.GetMouseButton (0)) {
+		brushImage.gameObject.SetActive (true);
 
-			Graphics.SetRenderTarget (renderTexture);
-			GL.PushMatrix ();
-			GL.LoadPixelMatrix (
-				textureImageRect.xMin, textureImageRect.xMax,
-				Screen.height - textureImageRect.yMin, Screen.height - textureImageRect.yMax);
+		Graphics.SetRenderTarget (renderTexture);
+		GL.PushMatrix ();
+		GL.LoadPixelMatrix (
+			textureImageRect.xMin, textureImageRect.xMax,
+			Screen.height - textureImageRect.yMin, Screen.height - textureImageRect.yMax);
 
-			Graphics.DrawTexture (
-				Rect.MinMaxRect (
-					brushWorldRect.xMin, Screen.height - brushWorldRect.yMax,
-					brushWorldRect.xMax, Screen.height - brushWorldRect.yMin),
-				brushImage.sprite.texture,
-				0, 0, 0, 0, eraseMaterial);	
+		Graphics.DrawTexture (
+			Rect.MinMaxRect (
+				brushWorldRect.xMin, Screen.height - brushWorldRect.yMax,
+				brushWorldRect.xMax, Screen.height - brushWorldRect.yMin),
+			brushImage.sprite.texture,
+			0, 0, 0, 0, eraseMaterial);	
 
-			Graphics.SetRenderTarget (null);
-			GL.PopMatrix ();
-		}
+		Graphics.SetRenderTarget (null);
+		GL.PopMatrix ();
 	}
 
 	public void SetBrushSprite (Sprite sprite)
